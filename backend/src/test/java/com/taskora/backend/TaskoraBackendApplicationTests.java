8caa0d43adb4d0e20c2d6ab60e8be41dcd5814f7
Shyamkumarnagilla <nagillashyamkumar7@gmail.com>
2026-01-23 15:00:59 +0530

    Initial backend setup for Taskora

---
+++ backend/src/test/java/com/taskora/backend/TaskoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
